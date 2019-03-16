30aec07478c1c1d8f9f9fae7d4bcc32b19d344d6
Ryan Linnit <ryan@ryanl.co.uk>
2019-03-16 12:47:53 +0000

    Fixed struct declaration

--- src/ifyt.h
+++ src/ifyt.h
@@ -6,7 +6,6 @@ int get_image_and_info(png_structp png_ptr, png_infop info_ptr,
 
 void print_image(png_bytep *row_pointers, png_uint_32 width,
     png_uint_32 height, int color_type, int truecolor);
-int is_blank(struct rgb values);
 
 struct rgb {
   int red;
@@ -15,6 +14,7 @@ struct rgb {
   int alpha;
 };
 
+int is_blank(struct rgb values);
 int get_ansi_color_code(struct rgb values);
 void print_truecolor_char(struct rgb row1, struct rgb row2);
 void print_ansi_char(int ansi_bg, int ansi_fg);
